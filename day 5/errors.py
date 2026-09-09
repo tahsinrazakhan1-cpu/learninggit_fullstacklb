2094f699930ae3bd1ca0651dddb8320cd62bdcb8
tahsinrazakhan1-cpu <tahsinrazakhan1@gmail.com>
2026-09-09 14:59:30 +0530

    meow

---
+++ day 5/errors.py
@@ -0,0 +1,14 @@
+# # # x,y = -19,7
+# # # print(x - int(x/y)*y)
+# # # print(x - x//y*y)
+# # # print(x%y)
+
+# print(7.5 / 2)
+# print(7.5 // 2)
+# print(7.5 % 2)
+
+# # print(450.0 % 360.0)
+# # print(26.5 % 24.0)
+# # print(3.75 % 1.0)
+# print(-7.5%-n n nm-2)
+# print(24//6.0)
